3867802444cbfb142693c8289c936c6ae8b08b6f
Eva Boogaard <evaboogaard@hotmail.com>
2022-12-07 13:32:58 +0100

    (style): styling homepage

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 // Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.scss';
 import '../scripts/cursor.js';
-
 import '../scripts/nav.js';
-
-console.log('Hello, world!');
+import '../scripts/content.js';
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,8 @@
 const hamburgerButton = document.querySelector('.hamburgerbutton');
 const navEl = document.querySelector('nav');
+const circle = document.getElementById('circle');
 
 hamburgerButton.addEventListener('click', function activeHamburger() {
   navEl.classList.toggle('active');
+  circle.classList.toggle('white');
 });
