2b29c03dc7b0e3da9193f896522659da883652a5
Eva Boogaard <evaboogaard@hotmail.com>
2023-05-23 19:56:27 +0200

    echt klaar mee wel

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,3 @@
-// Our bundler automatically creates styling when imported in the main JS file!
-import '../styles/style.scss';
 import '../scripts/nav.js';
 import '../scripts/content.js';
 import '../scripts/photography.js';
