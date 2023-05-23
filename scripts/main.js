48fe898e35dc64ab2617d4381404985de705ea1b
Eva Boogaard <evaboogaard@hotmail.com>
2023-05-23 17:26:11 +0200

    (add): photography

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 // Our bundler automatically creates styling when imported in the main JS file!
 import '../styles/style.scss';
-import '../scripts/cursor.js';
 import '../scripts/nav.js';
 import '../scripts/content.js';
+import '../scripts/photography.js';
+import '../scripts/cursor.js';
