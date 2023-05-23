224825e2ff80c78718688f71292502013de08510
Eva Boogaard <evaboogaard@hotmail.com>
2023-05-23 19:40:14 +0200

    build

--- dist/scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Our bundler automatically creates styling when imported in the main JS file!
-import '../styles/style.scss';
-import '../scripts/nav.js';
-import '../scripts/content.js';
-import '../scripts/photography.js';
-import '../scripts/cursor.js';
--- dist/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.querySelector('.hamburgerbutton');
-const navEl = document.querySelector('nav');
-const circle = document.getElementById('circle');
-
-hamburgerButton.addEventListener('click', function activeHamburger() {
-  navEl.classList.toggle('active');
-  circle.classList.toggle('white');
-});
